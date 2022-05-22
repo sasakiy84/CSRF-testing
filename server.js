4bbc8414c58697229c3b7bafdff4eb60dac03177
sasakiy84 <sasakiy84@g.ecc.u-tokyo.ac.jp>
2022-05-22 18:46:28 +0900

    add 3 example

--- server.js
+++ server.js
@@ -1,24 +1,53 @@
 import fastify from "fastify";
-// import { readFile } from "fs/promises";
 import fastifyStatic from "fastify-static";
+import fastifyFormbody from "fastify-formbody";
+import fastifyCookie from "@fastify/cookie";
+import fastifyCors from "@fastify/cors";
 import path from "path";
 
-const api = fastify({ logger: true });
+const api = fastify({
+  logger: {
+    prettyPrint: {
+      translateTime: "HH:MM:ss Z",
+      ignore: "pid,hostname",
+    },
+  },
+});
 const site = fastify({ logger: false });
 
 site.register(fastifyStatic, {
   root: path.join(process.cwd(), "html"),
 });
-
-// Declare a route
-api.get("/", async (request, reply) => {
-  api.log.info("data posted");
-  return { hello: "world" };
+api.register(fastifyFormbody);
+api.register(fastifyCors, {
+  origin: "http://localhost:3000",
+  credentials: true,
 });
-// site.get("/", async (request, reply) => {
-//   const html = await readFile("./html/1-get.html");
-//   reply.send(html);
-// });
+api.register(fastifyCookie, {
+  secret: "my-secret",
+  parseOptions: {},
+});
+// Declare a route
+api.get("/post-data", async (request, reply) => {
+  api.log.info("data posted");
+  return {};
+});
+api.post("/reset-password", async (request, reply) => {
+  api.log.info("reset password");
+  return "passward reseting succeeded";
+});
+api.get("/login", async (request, reply) => {
+  reply.setCookie("sessionid", "logined");
+  reply.send("logined");
+});
+api.post("/reset-password-with-cookie", async (request, reply) => {
+  if (request.cookies.sessionid !== "logined") {
+    api.log.info("password resseting failed");
+    return "not logined. passward resetting failed\nplease login http://127.0.0.1:4000/login";
+  }
+  api.log.info("password resseting succeeded");
+  return "passward reseting succeeded";
+});
 
 // Run the server!
 const start = async () => {
